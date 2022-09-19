84b822d8b23dcecc8f5d41ea879ed1a1dd1ff1b3
Diana <miac.stat@mail.ru>
2022-09-19 16:53:35 +0300

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse (Console.ReadLine());
+ 
+if(num1 > num2)
+{
+Console.WriteLine($"Первое число ({num1}) является большим, а второе ({num2}) - меньшим.");
+}
+else if (num1 < num2)
+{
+Console.WriteLine($"Второе число ({num2}) является большим, а первое ({num1}) - меньшим.");
+}
+else
+{
+Console.WriteLine("Оба числа равны.");
+}
